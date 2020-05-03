2851b67d2486c0ea7f01572c8ef84224420e19db
Jennie Brown <jennieabrown@gmail.com>
2020-05-02 20:59:28 -0400

    update curl

--- ucd/showProps.cmd
+++ ucd/showProps.cmd
@@ -1,5 +1,5 @@
 
 echo off
 set APP=%1
-echo "UCD environments for %APP%:"
-curl -x -h -u admin:admin https://zeus:8443/cli/application/environmentsInAppplication?application=%APP%
+echo UCD environments for %APP%:
+curl -s -k -u admin:admin "https://zeus:8443/cli/application/environmentsInApplication?application=%APP%"
